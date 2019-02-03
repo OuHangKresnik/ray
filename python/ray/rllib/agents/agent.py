9ff306ad2cd8d7f756f7b53c7cfae5367b9a56a7
ouhang.oh <ouhang.oh@antfin.com>
2019-02-03 19:12:44 +0800

    final

--- python/ray/rllib/agents/agent.py
+++ python/ray/rllib/agents/agent.py
@@ -564,10 +564,10 @@ class Agent(Trainable):
             input_creator = (lambda ioctx: MixedInput(config["input"], ioctx))
         elif config["input"].startswith("sls"):
             input_creator = (lambda ioctx: SlsReader(
-                merge_dict(dict(config["sls"]),{"train_batch_size" : config["train_batch_size"]}), ioctx))
+                merge_dicts(dict(config["sls"]),{"train_batch_size" : config["train_batch_size"]}), ioctx))
         elif config["input"].startswith("mock_sls"):
             input_creator = (lambda ioctx: MockSlsReader(
-                merge_dict(dict(config["sls"]),{"train_batch_size" : config["train_batch_size"]}), ioctx))  
+                merge_dicts(dict(config["sls"]),{"train_batch_size" : config["train_batch_size"]}), ioctx))  
         else:
             input_creator = (lambda ioctx: JsonReader(config["input"], ioctx))
 
