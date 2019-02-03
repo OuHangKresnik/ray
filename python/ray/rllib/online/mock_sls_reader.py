a0df2796d76d9103c30e437b3ab52ae0cd7120a8
ouhang.oh <ouhang.oh@antfin.com>
2019-02-03 18:44:48 +0800

    import missing

--- python/ray/rllib/online/mock_sls_reader.py
+++ python/ray/rllib/online/mock_sls_reader.py
@@ -18,6 +18,7 @@ except ImportError:
 from ray.rllib.offline.input_reader import InputReader
 from ray.rllib.evaluation.sample_batch import MultiAgentBatch, SampleBatch, \
     DEFAULT_POLICY_ID
+from ray.rllib.utils.annotations import override, PublicAPI    
 from ray.rllib.utils.compression import unpack_if_needed    
 
 logger = logging.getLogger(__name__)
